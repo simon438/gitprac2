6d3c661e96957a58bd6b4d7fc8529e5d9da55beb
Simon <simonstephenk99@gmail.com>
2023-07-15 11:41:54 +0530

    These files are not unique

---
+++ gorilla.sh
@@ -0,0 +1,3 @@
+Come to buy some hooks which are going to loose
+Then we will some get fortune
+
---
+++ jackie.java
@@ -0,0 +1,3 @@
+Wherever i go there is a machine learning
+Mostly we are in deep learning about 
+There we will be some jerks
