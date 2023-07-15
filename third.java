a3f777906706262bff020a53071301669ca7d7f0
Simon <simonstephenk99@gmail.com>
2023-07-15 13:58:08 +0530

    Revert "Updated file"
    This reverts commit e9f2016ba2ccf8d0c85ddb33d376ab73ad104fb7.

--- third.java
+++
@@ -1,4 +0,0 @@
-Mention some chices here are trust worthy
-Then we are not able to understand
-Where are some jerks i have to find and kill someone there are couldnot 
-be positive that nature helps you
